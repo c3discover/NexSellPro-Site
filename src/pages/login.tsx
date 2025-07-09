a338fbf8efd83e59b8e54126fcf895b57e025af7
c3discover <c3discover@gmail.com>
2025-07-09 15:21:58 -0400

    cleanup email login, still errors3

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase';
